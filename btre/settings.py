8f826142faa6930c5375c8595cd23aacd251e1e2
Mohammad Hakemi <hakemi.mohammad.22@gmail.com>
2022-02-19 17:22:34 +0330

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=srx0mfa(8vqqr*9xpk4*9a&@50@ulkl3c^75-i369++l84d&1'
+SECRET_KEY = '####'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
